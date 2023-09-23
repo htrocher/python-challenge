3ec368fc4992c99f86dc00e4d5d24ad1e32662a0
hugotroche <hugotroche@MacBook-Air.local>
2023-09-23 16:36:53 -0500

    comit after solving issues on Sept 23

---
+++ PyBank/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 18 12:15:04 2023
+
+@author: hugotroche
+"""
+
---
+++ PyPoll/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 18 12:18:05 2023
+
+@author: hugotroche
+"""
+
